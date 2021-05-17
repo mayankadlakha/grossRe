8a1b6ea1086c9829a0b52602bef2f660352e5307
Mayank Adlakha <mayankadlakha@gmail.com>
2021-05-17 20:36:16 +1000

    Move build to root

---
+++ build/precache-manifest.628d7161f90fddf49668bbda0aafd98e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a14290dd618ca1f4ca5364deda5ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531b26182624c2589ccc",
+    "url": "/static/js/2.a5246686.chunk.js"
+  },
+  {
+    "revision": "4a607267794850bef61493cced964fd9",
+    "url": "/static/js/2.a5246686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96627bd51d5b469810e5",
+    "url": "/static/js/main.25d013f5.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
